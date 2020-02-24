c1c5a17ef9b86c75aac0c5a581347617a2d956c2
m <m>
2020-02-24 16:45:19 +0000

    Tamagotchi Time
    setup psystem for virtual pet

---
+++ CTP App/Assets/Scripts/AvatarManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class AvatarManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public int Hunger = 0;
+    public int Boredom = 0;
+    public int Sleep = 0;
+    private float initialTime = 0;
+    private Slider hSlider;
+    private Slider bSlider;
+    private Slider sSlider;
+    private ParticleSystem mood;
+    private Image hFill;
+    private Image sFill;
+    private Image bFill;
+
+    private List<Color> colors;
+    void Start()
+    {
+        hSlider = GameObject.Find("Hunger").GetComponent<Slider>();
+        bSlider = GameObject.Find("Social").GetComponent<Slider>();
+        sSlider = GameObject.Find("Sleep").GetComponent<Slider>();
+       
+        mood = GameObject.Find("MoodPsystem").GetComponent<ParticleSystem>();
+        initialTime = Time.time;
+
+        hFill = GameObject.Find("HFill").GetComponent<Image>();
+        sFill = GameObject.Find("SFill").GetComponent<Image>();
+        bFill = GameObject.Find("Bored").GetComponent<Image>();
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hSlider.value = (float)Hunger;
+        bSlider.value = (float)Boredom;
+        sSlider.value = (float)Sleep;
+
+        int lowestMood;
+        if (Hunger < Boredom || Hunger < Sleep)
+        {
+            lowestMood = Hunger;
+            mood.startColor = hFill.color;
+      //      colors.Add(hFill.color);
+           
+        }
+
+        else if (Boredom < Hunger || Boredom < Sleep)
+        {
+            lowestMood = Boredom;
+            mood.startColor = bFill.color;
+        //    colors.Add(bFill.color);
+
+        }
+
+        else if (Sleep < Hunger || Sleep < Boredom)
+        {
+            lowestMood = Sleep;
+            mood.startColor = sFill.color;
+        //    colors.Add(sFill.color);
+        }
+    }
+}
---
+++ CTP App/Assets/Scripts/SaveManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public static class SaveManager
+{
+    // Start is called before the first frame update
+
+    public static void SaveAvatarStats(AvatarManager mainAvatar)
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream stream = new FileStream(Application.persistentDataPath + "/avatar.sav", FileMode.Create);
+        AvatarStats data = new AvatarStats(mainAvatar);
+
+        bf.Serialize(stream, data);
+        stream.Close();
+    }
+   
+}
+
+[Serializable]
+public class AvatarStats
+{
+    public int[] stats;
+
+    public AvatarStats(AvatarManager mainAvatar)
+    {
+        stats = new int[4];
+        stats[0] = mainAvatar.Boredom;
+        stats[1] = mainAvatar.Sleep;
+        stats[2] = mainAvatar.Hunger;
+
+
+    }
+}
